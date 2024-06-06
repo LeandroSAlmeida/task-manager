2cb5031ce822da8fa53bc8d15c3d915619556f05
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-06 04:51:44 -0300

    feat: create method insert in TaskService

--- src/main/java/com/project/task_manager/services/TaskService.java
+++ src/main/java/com/project/task_manager/services/TaskService.java
@@ -1,9 +1,34 @@
 package com.project.task_manager.services;
 
+import com.project.task_manager.domain.Task;
+import com.project.task_manager.dto.TaskDTO;
+import com.project.task_manager.repositories.TaskRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
 
+    @Autowired
+    private TaskRepository repository;
+
+    @Transactional
+    public TaskDTO insert(TaskDTO dto) {
+        Task entity = new Task();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new TaskDTO(entity);
+    }
+
+
+
+    private void copyDtoToEntity(TaskDTO dto, Task entity) {
+        entity.setTitle(dto.getTitle());
+        entity.setDescription(dto.getDescription());
+        entity.setFinishedAt(dto.getFinishedAt());
+        entity.setStatus(dto.getStatus());
+        entity.setPriority(dto.getPriority());
+    }
 
 }
