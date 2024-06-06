befc5ea12af3c7ac0d27236424e73cc06193f038
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-06 04:03:57 -0300

    feat: create Priority and Status Enum

---
+++ src/main/java/com/project/task_manager/domain/Priority.java
@@ -0,0 +1,7 @@
+package com.project.task_manager.domain;
+
+public enum Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/project/task_manager/domain/Status.java
@@ -0,0 +1,7 @@
+package com.project.task_manager.domain;
+
+public enum Status {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
