7ec38ceb9c6619b91b8fe3a9fa822f7e6bbd98b6
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-07 03:08:26 -0300

    feat: create method findAllTask in TaskService

--- src/main/java/com/project/task_manager/services/TaskService.java
+++ src/main/java/com/project/task_manager/services/TaskService.java
@@ -3,9 +3,12 @@ package com.project.task_manager.services;
 import com.project.task_manager.domain.Task;
 import com.project.task_manager.dto.TaskDTO;
 import com.project.task_manager.repositories.TaskRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
@@ -13,8 +16,14 @@ public class TaskService {
     @Autowired
     private TaskRepository repository;
 
+    @Transactional(readOnly = true)
+    public List<TaskDTO> findAllTask() {
+        List<Task> result = repository.findAll();
+        return  result.stream().map( x -> new TaskDTO(x)).collect(Collectors.toList());
+    }
+
     @Transactional
-    public TaskDTO insert(TaskDTO dto) {
+    public TaskDTO insertTask(TaskDTO dto) {
         Task entity = new Task();
         copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
