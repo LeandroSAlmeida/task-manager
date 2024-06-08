2bf8d4701722538bd90cc6932560a1d6d8e33d95
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-07 21:42:58 -0300

    fix: alter type in TaskRepository for Long

--- src/main/java/com/project/task_manager/repositories/TaskRepository.java
+++ src/main/java/com/project/task_manager/repositories/TaskRepository.java
@@ -3,5 +3,5 @@ package com.project.task_manager.repositories;
 import com.project.task_manager.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
