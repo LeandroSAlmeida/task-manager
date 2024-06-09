653f10ce66a900ef815e87da143ff33b7135b2f6
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-09 09:53:52 -0300

    feat: create method createTask in TaskController

--- src/main/java/com/project/task_manager/controllers/TaskController.java
+++ src/main/java/com/project/task_manager/controllers/TaskController.java
@@ -4,10 +4,10 @@ import com.project.task_manager.dto.TaskDTO;
 import com.project.task_manager.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +21,15 @@ public class TaskController {
     public ResponseEntity<List<TaskDTO>> findAllTasks(){
         List<TaskDTO> dto = service.findAllTask();
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO dto){
+        dto = service.insertTask(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+
     }
 
 }
