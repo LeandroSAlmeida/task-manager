426971856e2d4f9fd98f308447742c38d7ee1d73
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-10 05:23:49 -0300

    feat: adding exception handler for database in ControllerExceptionHandler

--- src/main/java/com/project/task_manager/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/project/task_manager/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.project.task_manager.controllers.handlers;
 
 import com.project.task_manager.dto.CustomError;
+import com.project.task_manager.services.exceptions.DatabaseException;
 import com.project.task_manager.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -18,4 +19,12 @@ public class ControllerExceptionHandler {
         CustomError err = new CustomError(Instant.now(),status.value(),e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(),status.value(),e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
