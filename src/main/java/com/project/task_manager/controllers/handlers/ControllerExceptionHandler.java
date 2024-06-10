fcbe937faf5146b7fe55f01da52e00a8d5c2e854
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-10 05:18:19 -0300

    feat: create ControllerExceptionHandler for better response

---
+++ src/main/java/com/project/task_manager/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.project.task_manager.controllers.handlers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    
+}
