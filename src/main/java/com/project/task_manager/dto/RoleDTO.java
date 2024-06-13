12debd5465a02e129906836452e1d423ad5eeae2
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-13 12:31:22 -0300

    feat: create RoleDTO

---
+++ src/main/java/com/project/task_manager/dto/RoleDTO.java
@@ -0,0 +1,29 @@
+package com.project.task_manager.dto;
+
+import com.project.task_manager.domain.Role;
+
+public class RoleDTO {
+
+    private Long id;
+    private String authority;
+
+    public RoleDTO() {
+    }
+
+    public RoleDTO(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+    public RoleDTO(Role entity) {
+        id = entity.getId();
+        authority = entity.getAuthority();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+}
