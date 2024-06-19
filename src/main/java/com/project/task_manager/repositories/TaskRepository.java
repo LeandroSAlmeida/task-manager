fc5709753b06b0eb6617f2e91f95093c51a9f97e
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-06-19 20:25:54 -0300

    feat: adding new consult in TaskRepository

--- src/main/java/com/project/task_manager/repositories/TaskRepository.java
+++ src/main/java/com/project/task_manager/repositories/TaskRepository.java
@@ -1,7 +1,12 @@
 package com.project.task_manager.repositories;
 
 import com.project.task_manager.domain.Task;
+import com.project.task_manager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser(User user);
 }
