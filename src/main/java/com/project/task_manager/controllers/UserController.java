38fbcfc3688d7d3b9f2ec05f0e5411cabba8cead
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-06-20 18:30:57 -0300

    feat: create method findAll in UserController

--- src/main/java/com/project/task_manager/controllers/UserController.java
+++ src/main/java/com/project/task_manager/controllers/UserController.java
@@ -3,14 +3,12 @@ package com.project.task_manager.controllers;
 import com.project.task_manager.dto.UserDTO;
 import com.project.task_manager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
