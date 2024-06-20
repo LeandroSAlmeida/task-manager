4a1c3d1d840130795d82f0ff7da2705909de7a61
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-06-20 18:26:34 -0300

    feat: create method findAll in UserController

--- src/main/java/com/project/task_manager/controllers/UserController.java
+++ src/main/java/com/project/task_manager/controllers/UserController.java
@@ -3,11 +3,15 @@ package com.project.task_manager.controllers;
 import com.project.task_manager.dto.UserDTO;
 import com.project.task_manager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -16,10 +20,17 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDTO> getMe() {
         UserDTO dto = service.getMe();
         return ResponseEntity.ok(dto);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
