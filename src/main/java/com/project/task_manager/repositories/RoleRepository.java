b1a13f56d175d0499b82766f0a93919583443d59
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-06-20 18:05:03 -0300

    feat: adding new query in RoleRepository

--- src/main/java/com/project/task_manager/repositories/RoleRepository.java
+++ src/main/java/com/project/task_manager/repositories/RoleRepository.java
@@ -4,4 +4,5 @@ import com.project.task_manager.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+     Role findByAuthority(String authority);
 }
